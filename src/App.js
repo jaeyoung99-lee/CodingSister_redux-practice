4719bfcc54d2f029e5affa4c9f50be1fac4571f5
jaeyoung99-lee <leejy0715@naver.com>
2024-08-24 19:01:13 +0900

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가!</button>
+    </div>
+  );
 }
 
 export default App;

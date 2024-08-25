f55e63f6b6979ffeb51f3704e1ecfc4d8a62e456
jaeyoung99-lee <leejy0715@naver.com>
2024-08-25 21:44:22 +0900

    Payload

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import Box from "./component/Box";
 
 function App() {
   const count = useSelector((state) => state.count);
+  const id = useSelector((state) => state.id);
+  const password = useSelector((state) => state.password);
   const dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({ type: "INCREMENT" });
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "1", password: "123" } });
   };
   return (
     <div>
       <h1>{count}</h1>
+      <h1>
+        {id}, {password}
+      </h1>
       <button onClick={increase}>증가!</button>
+      <button onClick={login}>Login</button>
       <Box />
     </div>
   );
